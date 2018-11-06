da406526bf28373426f929ef1aa8047fce367e15
Carter Barnes <carter.barnes@cremadevelopment.com>
2018-11-05 21:39:09 -0800

    Add Window - code

--- src/main.js
+++ src/main.js
@@ -1,7 +1,18 @@
 const electron = require('electron');
 
-const app = electron.app;
+const { app, BrowserWindow } = electron;
+
+let mainWindow = null;
 
 app.on('ready', _ => {
-    console.log('I\'m ready for anything!');
+  // Initialize our main browser window.
+  mainWindow = new BrowserWindow({});
+
+  // Load your root html file into the browser window.
+  mainWindow.loadURL(`file://${__dirname}/wiigf.html`);
+
+  // Cleanup after mainWindow is closed.
+  mainWindow.on('closed', _ => {
+    mainWindow = null;
+  });
 });
