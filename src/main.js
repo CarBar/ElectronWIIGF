fe379036b179024b5aa5cf03de4a101560969b7f
Carter Barnes <carter.barnes@cremadevelopment.com>
2018-11-07 13:59:40 -0800

    Add GlobalShortcuts - code

--- src/main.js
+++ src/main.js
@@ -1,34 +1,52 @@
 const electron = require('electron');
 const path = require('path');
 
-const { app, BrowserWindow, Menu, Tray } = electron;
+const { app, BrowserWindow, Menu, Tray, globalShortcut } = electron;
 
 let mainWindow = null;
 
-function buildTray()
-{
+function getFocus() {
+  console.log('Doing Show Main Window');
+  mainWindow.show();
+}
+
+function buildTray() {
   const tray = new Tray(path.join('src', 'SingleCupLogo.png'));
   const contextMenu = Menu.buildFromTemplate([
-      {
-          label: 'Thing 1',
-          click: _ => {
-              console.log('Clicked Thing 1');
-            }
-      },
-      {
-          label: 'Thing 2',
-          click: _ => {
-              console.log('Clicked Thing 2');
-          }
+    {
+      label: 'Thing 1',
+      click: _ => {
+        console.log('Clicked Thing 1');
       }
-  ])
+    },
+    {
+      label: 'Thing 2',
+      click: _ => {
+        console.log('Clicked Thing 2');
+      }
+    },
+    {
+      label: 'Show Main Window',
+      click: _ => {
+        getFocus();
+      }
+    }
+  ]);
   tray.setContextMenu(contextMenu);
   tray.setToolTip('Electron WIIGF');
+}
+
+function registerShortcuts(globalShortcut) {
+  globalShortcut.unregisterAll();
+  globalShortcut.register('CmdOrCtrl+Alt+C', _ => {
+    getFocus();
+  })
 }
 
 app.on('ready', _ => {
 
   buildTray();
+  registerShortcuts(globalShortcut);
 
   // Initialize our main browser window.
   mainWindow = new BrowserWindow({});
@@ -40,4 +58,10 @@ app.on('ready', _ => {
   mainWindow.on('closed', _ => {
     mainWindow = null;
   });
+
 });
+
+  // Cleanup when app closes
+  app.on('will-quit', _ => {
+    globalShortcut.unregisterAll();
+  });
