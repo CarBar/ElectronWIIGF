16c79571c4975c419de19e7ca66a73242e161795
Carter Barnes <carter.barnes@cremadevelopment.com>
2018-11-06 21:51:53 -0800

    Add Tray - code

--- src/main.js
+++ src/main.js
@@ -1,10 +1,35 @@
 const electron = require('electron');
+const path = require('path');
 
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, Menu, Tray } = electron;
 
 let mainWindow = null;
 
+function buildTray()
+{
+  const tray = new Tray(path.join('src', 'SingleCupLogo.png'));
+  const contextMenu = Menu.buildFromTemplate([
+      {
+          label: 'Thing 1',
+          click: _ => {
+              console.log('Clicked Thing 1');
+            }
+      },
+      {
+          label: 'Thing 2',
+          click: _ => {
+              console.log('Clicked Thing 2');
+          }
+      }
+  ])
+  tray.setContextMenu(contextMenu);
+  tray.setToolTip('Electron WIIGF');
+}
+
 app.on('ready', _ => {
+
+  buildTray();
+
   // Initialize our main browser window.
   mainWindow = new BrowserWindow({});
 
